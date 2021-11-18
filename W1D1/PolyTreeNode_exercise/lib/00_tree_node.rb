c24dd020a07d6b308fc18fc8b2310242f0cfc57a
TravTheBav <tzito93@gmail.com>
2021-11-18 12:34:38 -0600

    Nodes can delete children which updates the child node's parent attribute to nil

--- W1D1/PolyTreeNode_exercise/lib/00_tree_node.rb
+++ W1D1/PolyTreeNode_exercise/lib/00_tree_node.rb
@@ -9,14 +9,25 @@ class PolyTreeNode
 
     def parent=(parent_node)
         old_parent = parent
-        if !parent.equal?(parent_node) # failsafe incase parent is added twice
+        unless parent.equal?(parent_node) # failsafe incase parent is added twice
             @parent = parent_node
             return if parent_node.nil? # ends early if setting parent to nil
             parent_node.children << self # add self to new parents children
-            if !old_parent.nil? #remove self from old parents children if old parent not nil
+            unless old_parent.nil? #remove self from old parents children if old parent not nil
                 old_parent.children.delete(self) 
             end
         end
+    end
+
+    def add_child(child_node)
+        @children << child_node unless @children.include?(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "arg must be a child" if children.include?(child_node) == false
+        @children.delete(child_node)
+        child_node.parent = nil            
     end
 
 end
