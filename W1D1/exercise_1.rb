c56fc3e4af7d8c70d6293586b004b833e8e2408c
TravTheBav <tzito93@gmail.com>
2021-11-18 10:41:10 -0600

    Made Stack class for exercise_1.rb

---
+++ W1D1/exercise_1.rb
@@ -0,0 +1,21 @@
+class Stack
+    def initialize
+      # create ivar to store stack here!
+      @arr = []
+    end
+
+    def push(el)
+      # adds an element to the stack
+      @arr << el
+    end
+
+    def pop
+      # removes one element from the stack
+      @arr.pop
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      @arr[-1]
+    end
+end
