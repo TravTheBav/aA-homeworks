82e2da23f34061980543eaac97dfcd1051029a9e
TravTheBav <tzito93@gmail.com>
2021-11-29 11:20:47 -0600

    Completed tic_tac_toe Super AI project

--- W1D3/tic_tac_toe/lib/super_computer_player.rb
+++ W1D3/tic_tac_toe/lib/super_computer_player.rb
@@ -2,6 +2,23 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    case mark
+    when mark == :x
+      opponent = :o
+    else
+      opponent = :x
+    end
+
+    root_node = TicTacToeNode.new(game.board, mark)
+    root_node.children.each do |child_node|
+      if child_node.winning_node?(mark)
+        return child_node.prev_move_pos
+      end
+    end
+    # select from non-losing nodes if there are no winning nodes
+    not_losers = root_node.children.select { |child_node| !child_node.losing_node?(mark) }
+    raise "There should never be losing nodes" if not_losers.empty?
+    not_losers.sample.prev_move_pos
   end
 end
 
